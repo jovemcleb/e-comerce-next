bdeadd800aec78c6f155fed41c0b7935333d536c
Caleb Lima <lima.cardoso.caleb@gmail.com>
2024-04-16 18:05:30 -0300

    feat: structure initial in home

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,9 +1,50 @@
-export default async function Home() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+import Image from "next/image";
+import Link from "next/link";
 
+export default async function Home() {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+    <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
+      <Link
+        href="/"
+        className="group relative col-span-6 row-span-6 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          src="/moletom-ai-side.png"
+          alt=""
+          className="transition-transform duration-500 group-hover:scale-105"
+          width={780}
+          height={780}
+          quality={100}
+        />
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          src="/moletom-never-stop-learning.png"
+          alt=""
+          className="transition-transform duration-500 group-hover:scale-105"
+          width={380}
+          height={380}
+          quality={100}
+        />
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 flex items-end justify-center overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          src="/camiseta-dowhile-2022.png"
+          alt=""
+          className="transition-transform duration-500 group-hover:scale-105"
+          width={380}
+          height={380}
+          quality={100}
+        />
+      </Link>
+    </div>
   );
 }
